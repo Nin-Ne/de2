1e982fdcc12b24f1158ee212769463aed9dbf0ce
Nin-Ne <87557236+Nin-Ne@users.noreply.github.com>
2023-04-03 22:49:14 +0700

    Add files via upload

---
+++ Lop.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modul;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Lop {
+    int MaLop;
+    String TenLop;
+
+    public int getMaLop() {
+        return MaLop;
+    }
+
+    public void setMaLop(int MaLop) {
+        this.MaLop = MaLop;
+    }
+
+    public String getTenLop() {
+        return TenLop;
+    }
+
+    public void setTenLop(String TenLop) {
+        this.TenLop = TenLop;
+    }
+
+    public Lop(int MaLop, String TenLop) {
+        this.MaLop = MaLop;
+        this.TenLop = TenLop;
+    }
+    
+            
+}
